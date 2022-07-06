ccb599bd855f9b4139943ef51692a8756a829cf9
Tarantine Soares <tarantine@gmail.com>
2022-07-06 13:00:10 -0300

    User entity and resource

---
+++ src/main/java/com/educandowebcourse/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandowebcourse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandowebcourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "joão soares", "jão@gmail.com", "9999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
